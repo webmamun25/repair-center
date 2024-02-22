672609255e302cf02a8b1444adf752c2f861e2b4
webmamun25 <webmamun25@gmail.com>
2024-02-22 10:22:56 -0800

    final

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
 const app=express();
 const port =process.env.port || 4000;
 
-app.use(cors({
-  origin: '*',
-  credentials: true }));
+app.use(cors({origin: '*'}));
 app.use(express.json())
 
 
