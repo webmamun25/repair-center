12017416703b28264764ac30fb4b9af046210f61
webmamun25 <webmamun25@gmail.com>
2024-02-22 22:32:01 -0800

    new js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.json())
 
 
 
-console.log(process.env.MONGO_URI)
+console.log(process.env.MONGODB_URI)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGO_URI, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
