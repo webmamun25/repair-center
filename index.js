6c7f32a65c6e61f3cd8c63985112eb1216292c39
webmamun25 <webmamun25@gmail.com>
2024-02-23 20:33:55 -0800

    user change

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use(express.json())
 // Opx7SiZ5w5YHH6yy
 
 
-
-console.log(process.env.MONGODB_URI)
+MONGODB_URI=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7oueg7i.mongodb.net/?retryWrites=true&w=majority`
+console.log(MONGODB_URI)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGODB_URI, {
+const client = new MongoClient(MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
